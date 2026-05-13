0a33c19550a1aa2d45a6a685038867c6c9609662
Vinicius Rocha <vinicius.rocha2016@hotmail.com>
2026-05-13 11:21:49 -0300

    feat: Create postgre configs docker and identity seed

---
+++ src/Services/Identity/TicketSystem.Services.Identity/Infrastructure/Data/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TicketSystem.Services.Identity.Infrastructure.Data;
+
+public class AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : IdentityDbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(AppIdentityDbContext).Assembly);
+    }
+}
--- src/Services/Identity/TicketSystem.Services.Identity/Program.cs
+++ src/Services/Identity/TicketSystem.Services.Identity/Program.cs
@@ -1,17 +1,21 @@
 using Serilog;
 using TicketSystem.BuildingBlocks.Logging;
 using TicketSystem.BuildingBlocks.Observability;
+using TicketSystem.Services.Identity.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddCustomLogging();
 builder.Services.AddCustomTracing("Identity.API");
 
-builder.Services.AddControllers();
-
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services
+    .AddInfrastructureData(builder.Configuration)
+    .AddOpenApi()
+    .AddControllers();
+
+
 
 var app = builder.Build();
 
@@ -23,9 +27,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-
+app.EnsureSeedData();
 Log.Information("The Identity API is starting...");
-
 app.MapGet("/", () => "Identity.API is running!");
-
 app.Run();
